7aad086b12ac9611c469523757aaddffb2d94e19
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-09-02 20:26:18 -0500

    image and description toggling

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,39 +1,47 @@
 import React, { Component } from 'react';
-import { timingSafeEqual } from 'crypto';
 
 class GalleryItem extends Component {
     constructor(props) {
         super(props);
-        this.state = { isHidden: true };
+        this.state = {isHidden: false};
+        
     }
 
-    toggleHidden = () => {
-        this.setState({ isHidden: !this.state.isHidden })
+   toggleHidden = () => {
+       console.log('in toggleHidden');
+        this.setState({
+            isHidden: !this.isHidden,
+        })
 
     }
 
-    renderDescription = () => {
-        if (this.state.isHidden) {
-            return;
-        }
-        return (
-            <p>{this.props.galleryItem.description}</p>
-        )
-    }
     render() {
+        const {isHidden} = this.state;
         let message;
+        // let image;
         if (this.props.galleryItem.likes > 0) {
             message = <p>{this.props.galleryItem.likes} people have loved this photo</p>
         } else {
             message = <p>unloved</p>
         }
 
+        // if(this.state.isHidden === true) {
+        //     image = <p>{this.props.galleryItem.description}</p>
+        //  } else {
+        //     image = <img src={this.props.galleryItem.path} alt="goat"/>
+        //  }
+          
+
         return (
             <card>
-                <img src={this.props.galleryItem.path} onClick={this.toggleHidden} />
-                {!this.state.isHidden && <p>{this.props.galleryItem.description}</p>}
+                <div onClick={() => this.setState({isHidden: !isHidden})}>
+                {isHidden ? <p>{this.props.galleryItem.description}</p> : <img src={this.props.galleryItem.path} alt="goat"/>}
+                </div>
                 
-                <button onClick>Like</button>
+                {/* <div onClick={this.toggleHidden}>
+                {this.isHidden ? <p>{this.props.galleryItem.description}</p> : image = <img src={this.props.galleryItem.path} alt="goat"/>}
+                </div> */}
+                <button>Like</button>
                 {message}
             </card>
         );
